3594a0b041c3f445bbe485f54273f42fcdad4eda
ambishalsaha01 <bishalsaha9989@gmail.com>
2022-03-10 12:44:20 -0800

    fifth problem solve

--- js/app.js
+++ js/app.js
@@ -158,8 +158,8 @@ const displayLikedPosts = () => {
 };
 
 const displayReportedPosts = () => {
+    document.getElementById( "reported" ).innerHTML = '';
     const reportedPosts = getReportedPosts();
-    // console.log(reportedPosts)
     reportedPosts.forEach((post) => {
         const div = createPost(post);
         document.getElementById( "reported" ).appendChild(div);
